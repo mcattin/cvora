ddd07e3a9423f6a7b3b73ddc93ce7a70e63a272a
Matthieu Cattin <matthieu.cattin@cern.ch>
2013-11-26 15:59:27 +0100

    hdl: Update script with official gateware release location.

--- hdl/releases/release.sh
+++ hdl/releases/release.sh
@@ -6,7 +6,7 @@ S_XSVF='../prom/cvora.xsvf'
 D_BIT='cvora_v'$1'.bit'
 D_XSVF='cvora_v'$1'.xsvf'
 
-D_SCP='cs-ccr-dev3:/user/mcattin/cvora/firmware/'
+D_SCP='cs-ccr-dev3:/acc/local/share/firmware/cvora/'
 
 if echo $1 | egrep -q '^[1-9]?[0-9]_[0-9]{2}$'
 then
