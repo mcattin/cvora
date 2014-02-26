7cffe345ef3f2847b036172afdb4c72e503c1329
mcattin <mcattin@0a99fbf0-06a5-4c52-8a11-fe70044ee989>
2014-02-26 16:10:56 +0000

    all: Re-organise folder structure.
    Add synthesis makefile and release script.
    Remove xilinx/ folder (not used any more).
    Rename cpld project and sources folder.
    Move fpga related stuff to the hdl/ folder.
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/acc-fpga/trunk/fpga/cvora@78759 0a99fbf0-06a5-4c52-8a11-fe70044ee989

---
+++ hdl/releases/release.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+S_BIT='../syn/top_cvora.bit'
+S_XSVF='../prom/cvora.xsvf'
+
+D_BIT='cvora_v'$1'.bit'
+D_XSVF='cvora_v'$1'.xsvf'
+
+D_SCP='cs-ccr-dev3:/user/mcattin/cvora/firmware/'
+
+if echo $1 | egrep -q '^[1-9]?[0-9]_[0-9]{2}$'
+then
+    echo 'Releasing version '$1
+    cp -iv $S_BIT $D_BIT
+    cp -iv $S_XSVF $D_XSVF
+    scp $D_XSVF $D_SCP
+    ls -l
+else
+    echo 'Wrong version format!'
+fi
